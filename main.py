933917139b0da35049e2978d96c1aec726e00e42
Sam6900 <85671637+Sam6900@users.noreply.github.com>
2022-10-21 16:12:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
 
+min_down_speed, min_upload_speed = 100, 40
+chrome_driver_path = "C:/Development/chromedriver.exe"
+
+driver = webdriver.Chrome(service=Service(chrome_driver_path))
+driver.get("https://www.speedtest.net/")
+time.sleep(2)
+accept_cookies = driver.find_element("id", "onetrust-accept-btn-handler")
+accept_cookies.click()
+go_btn = driver.find_element("class name", "start-text")
+go_btn.click()
+time.sleep(40)
+
+download_speed = driver.find_element("class name", "download-speed")
+upload_speed = driver.find_element("class name", "upload-speed")
+if download_speed.text == "--" or upload_speed.text == "--":
+    time.sleep(20)
+    download_speed = driver.find_element("class name", "download-speed")
+    upload_speed = driver.find_element("class name", "upload-speed")
+
+driver.quit()
